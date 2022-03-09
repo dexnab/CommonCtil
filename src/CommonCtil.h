acd7a507b2cd7305c606353221610f7c07d4034d
dexnab <52860758+dexnab@users.noreply.github.com>
2022-03-09 17:36:18 +0800

    ftu_bign makefile

--- src/CommonCtil.h
+++ src/CommonCtil.h
@@ -1,6 +1,24 @@
-//（中文字符 卡utf-8编码用）
+/**
+* @file CommonCtil.h
+* @brief CommonCtil 项目管理模块；包含该文件即可使用 CommonCtil 的所有功能
+* @date 2022-02-22
+* @author dexnab
+* @author <李老师你把这里改成你的名字（github账号名）>
+*/
 #include <cmtType.h>
 
+/**
+* @typedef cmtConf
+* @brief CommonCtil项目配置信息
+* @see _CMTCONF
+* @date 2022-02-22
+* @author dexnab
+*/
+/**
+* @brief CommonCtil项目配置信息
+* @date 2022-02-22
+* @author dexnab
+*/
 typedef struct _CMTCONF
 {
 	cmtUint8 aes : 1;
@@ -28,5 +46,11 @@ typedef struct _CMTCONF
 	cmtUint8 rdtsc : 1;
 }cmtConf;
 
-//初始配置器
+/**
+* @brief CommonCtil 初始配置器；请在使用 CommonCtil 之前调用该函数
+* @pre @a locale 字符串必须有结束符
+* @test cmtDemoANSI() 状态：PA
+* @date 2021-09-23
+* @author dexnab
+*/
 extern void cmtConfInit();
